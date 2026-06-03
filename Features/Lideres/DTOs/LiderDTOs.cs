d25e77dcb1cb68100d7ac8d21f0dc49535970c42
Luis Fernando Sánchez Córdova <luis.sanchez@integritysolutions.com.ec>
2026-06-03 01:36:43 -0500

    Elisa change

--- Features/Lideres/DTOs/LiderDTOs.cs
+++
@@ -1,9 +0,0 @@
-namespace tmr_backend.Features.Lideres.DTOs;
-
-public record CrearLiderRequest(string Nombre, string Descripcion);
-
-public record LiderResponse(Guid Id, string Nombre, string Descripcion, bool Activo, DateTime FechaCreacion);
-
-public record ActualizarLiderRequest(string Nombre, string Descripcion);
-
-public record LiderLookupResponse(int Id, string Nombre);
