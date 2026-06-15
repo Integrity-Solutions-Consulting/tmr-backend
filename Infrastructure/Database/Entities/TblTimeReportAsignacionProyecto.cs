48779027b6607567fbba1bb849867daa745e7047
floresjeremy882-prog <floresjeremy882@gmail.com>
2026-06-14 23:41:01 -0500

    fix:entidad asignacion

---
+++ Infrastructure/Database/Entities/TblTimeReportAsignacionProyecto.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace tmr_backend.Infrastructure.Database.Entities;
+
+public partial class TblTimeReportAsignacionProyecto
+{
+    public int Id { get; set; }
+
+    public int? Idempleado { get; set; }
+
+    public int Idproyecto { get; set; }
+
+    public int? Idlider { get; set; }
+
+    public int? Idproveedor { get; set; }
+
+    public DateOnly? Fechaasignacion { get; set; }
+
+    public DateOnly? Fechafinasignacion { get; set; }
+
+    public string? Rolasignado { get; set; }
+
+    public decimal? Costoporhora { get; set; }
+
+    public decimal? Horasasignadas { get; set; }
+
+    public decimal? Lidercosto { get; set; }
+
+    public decimal? Liderhoras { get; set; }
+
+    public bool Activo { get; set; }
+
+    public string Usuariocreacion { get; set; } = null!;
+
+    public DateTime Fechacreacion { get; set; }
+
+    public string? Usuariomodificacion { get; set; }
+
+    public DateTime? Fechamodificacion { get; set; }
+
+    public string Ipcreacion { get; set; } = null!;
+
+    public string? Ipmodificacion { get; set; }
+
+    public virtual TblAdministracionEmpleado? IdempleadoNavigation { get; set; }
+
+    public virtual TblAdministracionLider? IdliderNavigation { get; set; }
+
+    public virtual TblTimeReportProyecto IdproyectoNavigation { get; set; } = null!;
+}
